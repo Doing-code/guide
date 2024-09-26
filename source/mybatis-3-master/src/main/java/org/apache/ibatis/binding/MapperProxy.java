7126c0d3664fbd75161150c1bd7e83def4bd87cb
yuanwu <2321550282@qq.com>
2024-09-26 21:17:10 +0800

    mybatis source parse

---
+++ source/mybatis-3-master/src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -0,0 +1,173 @@
+/*
+ *    Copyright 2009-2024 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.binding;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.apache.ibatis.reflection.ExceptionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.util.MapUtil;
+
+/**
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ */
+public class MapperProxy<T> implements InvocationHandler, Serializable {
+
+  private static final long serialVersionUID = -4724728412955527868L;
+  private static final int ALLOWED_MODES = MethodHandles.Lookup.PRIVATE | MethodHandles.Lookup.PROTECTED
+      | MethodHandles.Lookup.PACKAGE | MethodHandles.Lookup.PUBLIC;
+  private static final Constructor<Lookup> lookupConstructor;
+  private static final Method privateLookupInMethod;
+
+  // 记录了关联的 SqlSession 对象
+  private final SqlSession sqlSession;
+
+  // Mapper 接口的 Class 对象
+  private final Class<T> mapperInterface;
+
+  // 用于缓存 MapperMethod 对象，其中 key 是 Mapper 接口中方法对应的 Method 对象
+  // value 是 MapperMethod 对象。MapperMethod 对象会完成参数转换以及 SQL 语句的执行功能
+  // MapperMethod 中并不会记录任何状态相关的信息，所以可以在多个代理对象之间共享
+  private final Map<Method, MapperMethodInvoker> methodCache;
+
+  public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethodInvoker> methodCache) {
+    this.sqlSession = sqlSession;
+    this.mapperInterface = mapperInterface;
+    this.methodCache = methodCache;
+  }
+
+  static {
+    Method privateLookupIn;
+    try {
+      privateLookupIn = MethodHandles.class.getMethod("privateLookupIn", Class.class, MethodHandles.Lookup.class);
+    } catch (NoSuchMethodException e) {
+      privateLookupIn = null;
+    }
+    privateLookupInMethod = privateLookupIn;
+
+    Constructor<Lookup> lookup = null;
+    if (privateLookupInMethod == null) {
+      // JDK 1.8
+      try {
+        lookup = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
+        lookup.setAccessible(true);
+      } catch (NoSuchMethodException e) {
+        throw new IllegalStateException(
+            "There is neither 'privateLookupIn(Class, Lookup)' nor 'Lookup(Class, int)' method in java.lang.invoke.MethodHandles.",
+            e);
+      } catch (Exception e) {
+        lookup = null;
+      }
+    }
+    lookupConstructor = lookup;
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    try {
+      // 如果目标方法继承自 Object，则直接调用目标方法，如 toString()
+      if (Object.class.equals(method.getDeclaringClass())) {
+        return method.invoke(this, args);
+      }
+
+      // 从缓存中获取 MapperMethod 对象，如果缓存中没有，则创建新的 MapperMethod 对象并添加到缓存中
+      // 调用 MapperMethod#execute 方法执行 SQL 语句
+      return cachedInvoker(method).invoke(proxy, method, args, sqlSession);
+    } catch (Throwable t) {
+      throw ExceptionUtil.unwrapThrowable(t);
+    }
+  }
+
+  private MapperMethodInvoker cachedInvoker(Method method) throws Throwable {
+    try {
+      // 从缓存中获取 MapperMethod 对象，如果缓存中没有，则创建新的 MapperMethod 对象并添加到缓存中
+      return MapUtil.computeIfAbsent(methodCache, method, m -> {
+        if (!m.isDefault()) {
+          // 一般会执行这个构造器
+          return new PlainMethodInvoker(new MapperMethod(mapperInterface, method, sqlSession.getConfiguration()));
+        }
+
+        // 接口中 default 方法，针对 Java7 以上版本对动态类型语言的支持
+        try {
+          if (privateLookupInMethod == null) {
+            return new DefaultMethodInvoker(getMethodHandleJava8(method));
+          }
+          return new DefaultMethodInvoker(getMethodHandleJava9(method));
+        } catch (IllegalAccessException | InstantiationException | InvocationTargetException
+            | NoSuchMethodException e) {
+          throw new RuntimeException(e);
+        }
+      });
+    } catch (RuntimeException re) {
+      Throwable cause = re.getCause();
+      throw cause == null ? re : cause;
+    }
+  }
+
+  private MethodHandle getMethodHandleJava9(Method method)
+      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    final Class<?> declaringClass = method.getDeclaringClass();
+    return ((Lookup) privateLookupInMethod.invoke(null, declaringClass, MethodHandles.lookup())).findSpecial(
+        declaringClass, method.getName(), MethodType.methodType(method.getReturnType(), method.getParameterTypes()),
+        declaringClass);
+  }
+
+  private MethodHandle getMethodHandleJava8(Method method)
+      throws IllegalAccessException, InstantiationException, InvocationTargetException {
+    final Class<?> declaringClass = method.getDeclaringClass();
+    return lookupConstructor.newInstance(declaringClass, ALLOWED_MODES).unreflectSpecial(method, declaringClass);
+  }
+
+  interface MapperMethodInvoker {
+    Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable;
+  }
+
+  private static class PlainMethodInvoker implements MapperMethodInvoker {
+    private final MapperMethod mapperMethod;
+
+    public PlainMethodInvoker(MapperMethod mapperMethod) {
+      this.mapperMethod = mapperMethod;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
+      return mapperMethod.execute(sqlSession, args);
+    }
+  }
+
+  private static class DefaultMethodInvoker implements MapperMethodInvoker {
+    private final MethodHandle methodHandle;
+
+    public DefaultMethodInvoker(MethodHandle methodHandle) {
+      this.methodHandle = methodHandle;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args, SqlSession sqlSession) throws Throwable {
+      return methodHandle.bindTo(proxy).invokeWithArguments(args);
+    }
+  }
+}
---
+++ source/mybatis-3-master/src/main/java/org/apache/ibatis/binding/MapperProxyFactory.java
@@ -0,0 +1,60 @@
+/*
+ *    Copyright 2009-2024 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.binding;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.ibatis.binding.MapperProxy.MapperMethodInvoker;
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * @author Lasse Voss
+ */
+public class MapperProxyFactory<T> {
+
+  // 目标接口对象（JDK 动态代理，代理的是接口 interface），也就是 Mapper 接口对应的 Class 对象
+  private final Class<T> mapperInterface;
+
+  // 缓存，key 是 mapperInterface，Mapper 接口方法对应的 Method 对象,
+  // value 是对应的 MapperMethod 对象
+  private final Map<Method, MapperMethodInvoker> methodCache = new ConcurrentHashMap<>();
+
+  public MapperProxyFactory(Class<T> mapperInterface) {
+    this.mapperInterface = mapperInterface;
+  }
+
+  public Class<T> getMapperInterface() {
+    return mapperInterface;
+  }
+
+  public Map<Method, MapperMethodInvoker> getMethodCache() {
+    return methodCache;
+  }
+
+  @SuppressWarnings("unchecked")
+  protected T newInstance(MapperProxy<T> mapperProxy) {
+    return (T) Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[] { mapperInterface }, mapperProxy);
+  }
+
+  public T newInstance(SqlSession sqlSession) {
+    final MapperProxy<T> mapperProxy = new MapperProxy<>(sqlSession, mapperInterface, methodCache);
+    return newInstance(mapperProxy);
+  }
+
+}
